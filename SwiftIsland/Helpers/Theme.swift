5a0a79b203cccb4c29b8cb4d13a2d85d253df301
Paul Peelen <paul@paulpeelen.com>
2019-06-07 18:20:44 +0200

    Added theme file

---
+++ SwiftIsland/Helpers/Theme.swift
@@ -0,0 +1,42 @@
+//
+//  Theme.swift
+//  SwiftIsland
+//
+//  Created by Paul Peelen on 2019-06-07.
+//  Copyright © 2019 AppTrix AB. All rights reserved.
+//
+
+import UIKit
+
+enum Theme: String {
+  case tag
+  case tagFont
+  case scheduleTimeline
+  case scheduleTimelineFont
+  case textBody
+  case textDefault
+
+  case yellowLight
+  case yellow
+  case orangeLight
+  case orange
+  case red
+  case redDark
+}
+
+extension Theme {
+
+  var color: UIColor {
+    guard let instanceColor = UIColor(named: self.rawValue) else {
+      return .clear
+    }
+
+    return instanceColor
+  }
+}
+
+extension UIColor {
+  static func themeColor(_ theme: Theme) -> UIColor {
+    return theme.color
+  }
+}
