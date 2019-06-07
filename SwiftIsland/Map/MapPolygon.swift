4024da2f65b98da17cb0e682d1682d445df8bafe
Paul Peelen <paul@paulpeelen.com>
2019-06-07 18:22:09 +0200

    Added support for polygons for cottages and search

---
+++ SwiftIsland/Map/MapPolygon.swift
@@ -0,0 +1,47 @@
+//
+//  MapPolygon.swift
+//  SwiftIsland
+//
+//  Created by Paul Peelen on 2019-06-07.
+//  Copyright © 2019 AppTrix AB. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class MapPolygon: MKPolygonRenderer {
+
+  let area: Area
+  let selected: Bool
+
+  init?(cottage: CottagePolygon) {
+    guard let area = cottage.cottageArea else { return nil }
+    self.area = area
+    self.selected = cottage.selected
+    super.init(polygon: cottage)
+
+    fillColor = .themeColor(.yellowLight)
+    strokeColor = .themeColor(.orange)
+    lineWidth = 2
+  }
+
+  override func draw(_ mapRect: MKMapRect, zoomScale: MKZoomScale, in context: CGContext) {
+    super.draw(mapRect, zoomScale: zoomScale, in: context)
+
+    let paragraphStyle = NSMutableParagraphStyle()
+    paragraphStyle.alignment = .center
+    let attributes = [NSAttributedString.Key.paragraphStyle  : paragraphStyle,
+                      NSAttributedString.Key.font            : UIFont.systemFont(ofSize: 50.0),
+                      NSAttributedString.Key.foregroundColor : UIColor.themeColor(.redDark)]
+    let polyRect = self.rect(for: polygon.boundingMapRect)
+
+    UIGraphicsPushContext(context)
+    "\(area.name)".draw(in: polyRect, withAttributes: attributes)
+    UIGraphicsPopContext()
+  }
+}
+
+class CottagePolygon: MKPolygon {
+  var cottageArea: Area?
+  var selected: Bool = true
+}
--- SwiftIsland/Map/MapViewController.swift
+++ SwiftIsland/Map/MapViewController.swift
@@ -12,9 +12,25 @@ import MapKit
 class MapViewController: UIViewController {
 
   private let dataManager = DataManager.shared
-  private var areas: [Area] = []
+  private var areas: [Area] = [] {
+  }
+  private var selectedArea: Area? {
+    didSet {
+      if let selectedArea = selectedArea {
+        let region = MKCoordinateRegion(center: selectedArea.center,
+                                        span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001))
+        mapView.setRegion(region, animated: true)
+      }
+    }
+  }
 
   @IBOutlet weak var mapView: MKMapView!
+  @IBOutlet weak var cottagePickerView: UIView! {
+    didSet {
+      cottagePickerView.layer.cornerRadius = 12
+    }
+  }
+  @IBOutlet weak var cottagePicker: UIPickerView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -34,6 +50,7 @@ class MapViewController: UIViewController {
     dataManager.getArea { result in
       switch result {
       case .success(let areas):
+        let areas = areas.sorted { $0.name < $1.name }
         self.setupMapOverlays(areas: areas)
       case .failure(let error):
         debugPrint("Error fetching areas: \(error.localizedDescription)")
@@ -44,12 +61,16 @@ class MapViewController: UIViewController {
   private func setupMapOverlays(areas: [Area]) {
     mapView.removeOverlays(mapView.overlays)
     self.areas = areas
+    cottagePicker.reloadAllComponents()
+    renderMapOverlays()
+  }
 
+  private func renderMapOverlays() {
     for area in areas {
-      let polygon = MKPolygon(coordinates: area.locationCoordinate2D, count: area.locationCoordinate2D.count)
+      let polygon = CottagePolygon(coordinates: area.locationCoordinate2D, count: area.locationCoordinate2D.count)
+      polygon.cottageArea = area
+      polygon.selected = selectedArea == nil ? true : selectedArea?.name == area.name
       mapView.addOverlay(polygon)
-
-      // Add outer line
     }
   }
 
@@ -78,11 +99,43 @@ extension MapViewController: MKMapViewDelegate {
       polylineRenderer.strokeColor = .orange
       polylineRenderer.lineWidth = 5
       return polylineRenderer
-    } else if overlay is MKPolygon {
-      let polygonView = MKPolygonRenderer(overlay: overlay)
-      polygonView.fillColor = .magenta
+    } else if let cottageOverlay = overlay as? CottagePolygon, let polygonView = MapPolygon(cottage: cottageOverlay) {
+      polygonView.alpha = polygonView.selected ? 1 : 0.5
       return polygonView
     }
+
     return MKPolylineRenderer(overlay: overlay)
+  }
+}
+
+extension MapViewController: UIPickerViewDelegate {
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    if row == 0 {
+      selectedArea = nil
+    } else {
+      let area = areas[row-1]
+      selectedArea = area
+    }
+    mapView.removeOverlays(mapView.overlays)
+    renderMapOverlays()
+  }
+}
+
+extension MapViewController: UIPickerViewDataSource {
+
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return areas.count + 1
+  }
+
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    if row == 0 {
+      return ""
+    }
+
+    return areas[row-1].name
   }
 }
