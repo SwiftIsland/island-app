2b701725015fbba5d3c007c3c8bd2cedc0209528
Paul Peelen <paul@paulpeelen.com>
2019-06-08 23:54:44 +0200

    Update for corners on card

--- SwiftIsland/Utilities/Extensions/UIViewExtensions.swift
+++ SwiftIsland/Utilities/Extensions/UIViewExtensions.swift
@@ -55,11 +55,4 @@ extension UIView {
       layer.borderColor = newValue?.cgColor
     }
   }
-
-  func roundCorners(corners: UIRectCorner, radius: CGFloat) {
-    let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-    let mask = CAShapeLayer()
-    mask.path = path.cgPath
-    layer.mask = mask
-  }
 }
