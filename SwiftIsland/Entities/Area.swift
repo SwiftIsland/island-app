9a51df9bfc8099d61aa1e5e6a8946b37d364a9c7
Paul Peelen <Paul@PaulPeelen.com>
2019-06-09 22:33:40 +0200

    Feature/map polygons (#6)
    * Updated for correct Area with hotel and first cottage
    
    * Added theme file
    
    * Updated the areas (cottages location)
    
    * Added support for polygons for cottages and search
    
    * Minor bugfix

--- SwiftIsland/Entities/Area.swift
+++ SwiftIsland/Entities/Area.swift
@@ -6,24 +6,79 @@
 //  Copyright © 2019 AppTrix AB. All rights reserved.
 //
 
-import Foundation
 import CoreLocation
+import GLKit
 
 struct Area: Decodable {
   let name: String
-  let coordinates: [CLLocationCoordinate2D]
+  let points: [Point]
+  let locationCoordinate2D: [CLLocationCoordinate2D]
+
+  var center: CLLocationCoordinate2D {
+    var x: Float = 0.0
+    var y: Float = 0.0
+    var z: Float = 0.0
+    for points in locationCoordinate2D {
+      let lat = GLKMathDegreesToRadians(Float(points.latitude))
+      let long = GLKMathDegreesToRadians(Float(points.longitude))
+
+      x += cos(lat) * cos(long)
+      y += cos(lat) * sin(long)
+      z += sin(lat)
+    }
+    x = x / Float(locationCoordinate2D.count)
+    y = y / Float(locationCoordinate2D.count)
+    z = z / Float(locationCoordinate2D.count)
+
+    let resultLong = atan2(y, x)
+    let resultHyp = sqrt(x * x + y * y)
+    let resultLat = atan2(z, resultHyp)
+    let result = CLLocationCoordinate2D(latitude: CLLocationDegrees(GLKMathRadiansToDegrees(Float(resultLat))),
+                                        longitude: CLLocationDegrees(GLKMathRadiansToDegrees(Float(resultLong))))
+    return result
+  }
+
+  struct Point: Codable {
+    var latitude: Double {
+      return coordinate.latitude
+    }
+    var longitude: Double {
+      return coordinate.longitude
+    }
+    let coordinate: CLLocationCoordinate2D
+
+    public enum CodingKeys: String, CodingKey {
+      case latitude
+      case longitude
+    }
+
+    public func encode(to encoder: Encoder) throws {
+      var container = encoder.container(keyedBy: CodingKeys.self)
+      try container.encode(latitude, forKey: .latitude)
+      try container.encode(longitude, forKey: .longitude)
+    }
+
+    public init(from decoder: Decoder) throws {
+      let values = try decoder.container(keyedBy: CodingKeys.self)
+      let latitude = try values.decode(Double.self, forKey: .latitude)
+      let longitude = try values.decode(Double.self, forKey: .longitude)
+
+      coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+  }
 
   enum CodingKeys: String, CodingKey {
     case name = "name"
-    case coordinates = "coordinates"
+    case points = "points"
   }
 
   init(from decoder: Decoder) throws {
     let values = try decoder.container(keyedBy: CodingKeys.self)
     name = try values.decode(String.self, forKey: .name)
+    let points = try values.decode([Point].self, forKey: .points)
+    self.points = points
 
-    let coordinates = try values.decode([Double].self, forKey: .coordinates)
-
-    self.coordinates = []
+    let locationCoordinate2D = points.compactMap({ $0.coordinate })
+    self.locationCoordinate2D = locationCoordinate2D
   }
 }
--- SwiftIsland/Map/MapViewController.swift
+++ SwiftIsland/Map/MapViewController.swift
@@ -13,26 +13,46 @@ class MapViewController: UIViewController {
 
   private let dataManager = DataManager.shared
   private var areas: [Area] = []
+  private var selectedArea: Area? {
+    didSet {
+      if let selectedArea = selectedArea {
+        let region = MKCoordinateRegion(center: selectedArea.center,
+                                        span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001))
+        mapView.setRegion(region, animated: true)
+      }
+    }
+  }
 
   @IBOutlet weak var mapView: MKMapView!
+  @IBOutlet weak var cottagePickerView: UIView! {
+    didSet {
+      cottagePickerView.layer.cornerRadius = 12
+    }
+  }
+  @IBOutlet weak var cottagePicker: UIPickerView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
     setupMap()
   }
 
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    getAreas()
+  }
+
   private func setupMap() {
     centerOnVenue()
-    getAreas()
   }
 
   private func getAreas() {
     dataManager.getArea { result in
       switch result {
       case .success(let areas):
+        let areas = areas.sorted { $0.name < $1.name }
         self.setupMapOverlays(areas: areas)
       case .failure(let error):
-        debugPrint(error.localizedDescription)
+        debugPrint("Error fetching areas: \(error.localizedDescription)")
       }
     }
   }
@@ -40,18 +60,27 @@ class MapViewController: UIViewController {
   private func setupMapOverlays(areas: [Area]) {
     mapView.removeOverlays(mapView.overlays)
     self.areas = areas
-//    let locations = areas.map { $0.coordinates }
-//    let polygon = MKPolygon(
+    cottagePicker.reloadAllComponents()
+    renderMapOverlays()
+  }
+
+  private func renderMapOverlays() {
+    for area in areas {
+      let polygon = CottagePolygon(coordinates: area.locationCoordinate2D, count: area.locationCoordinate2D.count)
+      polygon.cottageArea = area
+      polygon.selected = selectedArea == nil ? true : selectedArea?.name == area.name
+      mapView.addOverlay(polygon)
+    }
   }
 
   private func centerOnVenue() {
-    let location = CLLocation(latitude: 53.1148856, longitude: 4.895983)
+    let location = CLLocation(latitude: 53.11492071953518, longitude: 4.89718462979863)
     centerMapOnLocation(location: location)
   }
 
   func centerMapOnLocation(location: CLLocation) {
     let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude),
-                                    span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+                                    span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
     DispatchQueue.main.async {
       self.mapView.setRegion(region, animated: true)
       let annotation = MKPointAnnotation()
@@ -61,4 +90,51 @@ class MapViewController: UIViewController {
   }
 }
 
-extension MapViewController: MKMapViewDelegate { }
+extension MapViewController: MKMapViewDelegate {
+  
+  func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+    if overlay is MKPolyline {
+      let polylineRenderer = MKPolylineRenderer(overlay: overlay)
+      polylineRenderer.strokeColor = .orange
+      polylineRenderer.lineWidth = 5
+      return polylineRenderer
+    } else if let cottageOverlay = overlay as? CottagePolygon, let polygonView = MapPolygon(cottage: cottageOverlay) {
+      polygonView.alpha = polygonView.selected ? 1 : 0.5
+      return polygonView
+    }
+
+    return MKPolylineRenderer(overlay: overlay)
+  }
+}
+
+extension MapViewController: UIPickerViewDelegate {
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    if row == 0 {
+      selectedArea = nil
+    } else {
+      let area = areas[row-1]
+      selectedArea = area
+    }
+    mapView.removeOverlays(mapView.overlays)
+    renderMapOverlays()
+  }
+}
+
+extension MapViewController: UIPickerViewDataSource {
+
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return areas.count + 1
+  }
+
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    if row == 0 {
+      return ""
+    }
+
+    return areas[row-1].name
+  }
+}
