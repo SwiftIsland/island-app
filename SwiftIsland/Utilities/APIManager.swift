8954ccc38f9bc3b86480b8fce7bdf984297678b4
Paul Peelen <paul@paulpeelen.com>
2019-06-16 23:20:23 +0200

    Added the APIManager

--- SwiftIsland/Utilities/APIManager.swift
+++ SwiftIsland/Utilities/APIManager.swift
@@ -8,12 +8,79 @@
 
 import Foundation
 
-protocol APIManaging { }
-
-enum Endpoints: String {
-  case first
+enum APIManagerError: Error, Equatable {
+  case invalidData
+  case apiReponseUnhandledStatusCode(statusCode: Int)
 }
 
-class APIManager { }
+protocol APIManaging {
+  func get<T: Decodable>(endpoint: APIManager.Endpoint, completionHandler: @escaping ((Result<T, Error>) -> Void)) -> URLSessionTask?
+}
 
-extension APIManager: APIManaging { }
+final class APIManager: APIManaging {
+
+  enum Endpoint: String {
+    case schedule
+    case mentors
+    case area = "locations"
+  }
+
+  private let baseURL: URL
+  private let urlSession: URLSession
+
+  init(baseURL: URL, urlSession: URLSession = URLSession.shared) {
+    self.baseURL = baseURL
+    self.urlSession = urlSession
+  }
+
+  func get<T: Decodable>(endpoint: Endpoint, completionHandler: @escaping ((Result<T, Error>) -> Void)) -> URLSessionTask? {
+    return request(endpoint: endpoint, completionHandler: completionHandler)
+  }
+
+  // MARK: - Private
+
+  private func request<T: Decodable>(endpoint: Endpoint, completionHandler: @escaping ((Result<T, Error>) -> Void)) -> URLSessionTask? {
+
+    let completionHandlerOnMain: ((Result<T, Error>) -> Void) = { result in
+      DispatchQueue.main.async {
+        completionHandler(result)
+      }
+    }
+
+    return request(endPoint: endpoint) { (data, response, error) in
+      if let error = error {
+        completionHandlerOnMain(.failure(error))
+        return
+      }
+
+      if let urlResponse = response as? HTTPURLResponse, !(200..<300).contains(urlResponse.statusCode) {
+        completionHandlerOnMain(.failure(APIManagerError.apiReponseUnhandledStatusCode(statusCode: urlResponse.statusCode)))
+        return
+      }
+
+      guard let data = data else {
+        completionHandlerOnMain(.failure(APIManagerError.invalidData))
+        return
+      }
+
+      let decoder = JSONDecoder()
+
+      do {
+        let decoded = try decoder.decode(T.self, from: data)
+        completionHandlerOnMain(.success(decoded))
+      } catch {
+        completionHandlerOnMain(.failure(error))
+      }
+    }
+  }
+
+  private func request(endPoint: Endpoint, completionHandler: @escaping ((Data?, URLResponse?, Error?) -> Void)) -> URLSessionDataTask? {
+    var request = URLRequest(url: baseURL.appendingPathComponent(endPoint.rawValue))
+    request.httpMethod = "GET"
+
+    let task = urlSession.dataTask(with: request, completionHandler: completionHandler)
+    task.resume()
+
+    return task
+  }
+}
---
+++ SwiftIslandTests/Mocks/URLSessionMock.swift
@@ -0,0 +1,43 @@
+//
+//  URLSessionMock.swift
+//  ICATests
+//
+//  Copyright © 2018 ICA Gruppen. All rights reserved.
+//
+
+import Foundation
+
+final class URLSessionMock : URLSession {
+
+  var dataTaskInvokeCount = 0
+  var dataTaskRequest: URLRequest?
+  var dataTaskCompletionHandler: ((Data?, URLResponse?, Error?) -> Void)?
+  var dataTaskReturnUrlSessionDataTask: URLSessionDataTaskMock = URLSessionDataTaskMock()
+
+  var didCallDataTaskWithRequestCompletionHandler: (() -> Void)?
+
+  var invalidateAndCancelInvokeCount = 0
+
+  override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+    dataTaskInvokeCount += 1
+    dataTaskRequest = request
+    dataTaskCompletionHandler = completionHandler
+
+    didCallDataTaskWithRequestCompletionHandler?()
+
+    return dataTaskReturnUrlSessionDataTask
+  }
+
+  override func invalidateAndCancel() {
+    invalidateAndCancelInvokeCount += 1
+  }
+}
+
+final class URLSessionDataTaskMock : URLSessionDataTask {
+
+  var resumeInvokeCount = 0
+
+  override func resume() {
+    resumeInvokeCount += 1
+  }
+}
