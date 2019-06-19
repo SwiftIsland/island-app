e7e612fe86d86c7399d2febc10f21c45ea20a48b
Paul Peelen <Paul@PaulPeelen.com>
2019-06-19 13:19:02 +0200

    Requested ui updates (#19)
    * UI Updates for main page
    
    * Fix for mentor page bottom magin
    
    * Tabbar color updates
    
    * Set image inset for tabbar
    
    * Change of main view
    
    * Removed unused file
    
    * Change for tabbar colors
    
    * Make sure no seperators are shown when not having content
    
    * Added social buttons
    
    * Added ITSAppUsesNonExemptEncryption

--- SwiftIsland/Mentor/MentorCardViewController.swift
+++ SwiftIsland/Mentor/MentorCardViewController.swift
@@ -17,13 +17,15 @@ class MentorCardViewController: UIViewController {
 
   @IBOutlet weak var titleLabel: UILabel!
   @IBOutlet weak var descriptionLabel: UILabel!
-  @IBOutlet weak var timeLabel: UILabel!
-  @IBOutlet weak var locationLabel: UILabel!
+  @IBOutlet weak var socialTwitter: UIButton!
+  @IBOutlet weak var socialWeb: UIButton!
+
+  private var mentor: Mentor?
 
   func setup(withMentor mentor: Mentor) {
+    self.mentor = mentor
     titleLabel.text = mentor.name
     descriptionLabel.text = mentor.bio
-//    locationLabel.text = activity.area
 
     if let image = UIImage(named: mentor.image) {
       mentorImage.image = image
@@ -31,5 +33,20 @@ class MentorCardViewController: UIViewController {
     } else {
       mentorImageView.isHidden = true
     }
+
+    socialTwitter.alpha = mentor.twitter != nil ? 1 : 0.3
+    socialTwitter.isUserInteractionEnabled = mentor.twitter != nil
+    socialWeb.alpha = mentor.web != nil ? 1 : 0.3
+    socialWeb.isUserInteractionEnabled = mentor.twitter != nil
+  }
+
+  @IBAction func openTwitterUrl(_ sender: UIButton) {
+    guard let twitterUrl = mentor?.twitter else { return }
+    UIApplication.shared.open(twitterUrl)
+  }
+
+  @IBAction func openWeb(_ sender: UIButton) {
+    guard let webUrl = mentor?.web else { return }
+    UIApplication.shared.open(webUrl)
   }
 }
