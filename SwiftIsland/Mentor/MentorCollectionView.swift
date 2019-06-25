007450234a882d4a540ec56119fc9fe17b8e99f1
Bruno Scheele <bruno@noodlewerk.com>
2019-06-25 21:27:58 +0200

    Mentor cell accessibility (#34)
    * Add accessibility to mentor cell.
    
    * Set name and country label font to a text style.
    * Enable 'Change dynamically'.
    * Put views in stackview instead.
    * Use cell's content view instead custom view.
    * Invalidate layout on trait change.
    
    * Adjust estimated item size on larger test styles.
    
    * Use font from original design.
    
    * Fix compatibility with Xcode 10. Oops.
    
    Too bad this breaks proper sizing of the collection view cell.
    
    * Put font setup in prepare for reuse. Just in case.
    
    * Move mentor screens to their own view.
    
    * Looks better, but definitely not according to design.
    
    * Boooh, self-sizing! Booh! 👻
    
    This puts the name and country in the same label, which simplifies the internal calculations. It also drops self-sizing in favor of artisanally crafted calculations! 👌

--- SwiftIsland/Mentor/MentorCollectionView.swift
+++ SwiftIsland/Mentor/MentorCollectionView.swift
@@ -24,15 +24,35 @@ class MentorCollectionView: CardViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     fetchMentors()
-
+    setupCollectionView()
+  }
+  
+  override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+    super.traitCollectionDidChange(previousTraitCollection)
+    
+    guard previousTraitCollection != nil else {
+      return
+    }
+    collectionView?.collectionViewLayout.invalidateLayout()
+    setupCollectionView()
+  }
+  
+  private func setupCollectionView() {
     if let flowLayout = collectionView?.collectionViewLayout as? UICollectionViewFlowLayout {
+      
+      let frameWidth = view.frame.width
       let horizontalSpacing = flowLayout.scrollDirection == .vertical ? flowLayout.minimumInteritemSpacing : flowLayout.minimumLineSpacing
-      let cellDifference = flowLayout.itemSize.height / flowLayout.itemSize.width
-      let cellWidth = (view.frame.width - max(0, numberOfCellsPerRow - 1)*horizontalSpacing)/numberOfCellsPerRow
-      flowLayout.itemSize = CGSize(width: cellWidth, height: cellWidth * cellDifference)
+      let isAccessibility = traitCollection.preferredContentSizeCategory.isAccessibilityCategory
+      let ratio: CGFloat = isAccessibility ? 1 : 1.6
+      let numberOfCellsPerRow = traitCollection.preferredContentSizeCategory.isAccessibilityCategory ? 1 : 2
+      let totalSpacing = horizontalSpacing * CGFloat(numberOfCellsPerRow - 1)
+      let padding = CGFloat(12 * 2) // padding left and right
+      let cellWidth = (frameWidth - padding - totalSpacing) / CGFloat(numberOfCellsPerRow)
+      
+      flowLayout.itemSize = CGSize(width: cellWidth, height: cellWidth * ratio)
     }
   }
-
+  
   private func fetchMentors() {
     loadingSpinner.startAnimating()
     dataManager.getMentors { result in
--- SwiftIsland/Mentor/MentorCollectionViewCell.swift
+++ SwiftIsland/Mentor/MentorCollectionViewCell.swift
@@ -11,25 +11,56 @@ import UIKit
 class MentorCollectionViewCell: UICollectionViewCell {
 
   @IBOutlet weak var mentorImage: UIImageView!
-  @IBOutlet weak var nameLabel: UILabel!
-  @IBOutlet weak var countryLabel: UILabel!
-
+  @IBOutlet weak var label: UILabel!
+  @IBOutlet weak var imageWidthConstraint: NSLayoutConstraint!
+    
   override func prepareForReuse() {
     super.prepareForReuse()
     mentorImage.image = nil
-    nameLabel.text = nil
+    label.text = nil
+    setupFonts()
+  }
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    setupFonts()
+    setupMetrics(with: traitCollection)
   }
 
+  override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+    super.traitCollectionDidChange(previousTraitCollection)
+    guard previousTraitCollection != nil else {
+      return
+    }
+    setupFonts()
+  }
+  
   func setup(withMentor mentor: Mentor) {
     if let image = UIImage(named: mentor.image) {
       mentorImage.image = image
     }
-    nameLabel.text = mentor.name
-
-    if let country = mentor.country {
-      countryLabel.text = country
+    let text: String = {
+      var result = mentor.name
+      if let country = mentor.country {
+        result += "\n\(country)"
+      }
+      return result
+    }()
+    label.text = text
+  }
+  
+  private func setupMetrics(with traits: UITraitCollection) {
+    if traits.preferredContentSizeCategory.isAccessibilityCategory {
+      imageWidthConstraint.constant = 240
     } else {
-      countryLabel.text = ""
+      imageWidthConstraint.constant = 136
     }
+  }
+  
+  private func setupFonts() {
+    let font = UIFont.systemFont(ofSize: 18.0, weight: .light)
+    let metrics = UIFontMetrics(forTextStyle: .headline)
+    let newFont = metrics.scaledFont(for: font)
+    label.font = newFont
   }
 }
