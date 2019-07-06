98b9e61c45416d8ae2d53be439591c90f3a62791
Paul Peelen <paul@paulpeelen.com>
2019-07-06 13:17:19 +0200

    Removed api key

--- fastlane/constants.rb
+++ fastlane/constants.rb
@@ -12,10 +12,5 @@ CONFIG_ADHOC = 'AdHoc'
 # Branches that are deployable
 BRANCHES_DEPLOY = ["master", "develop"]
 
-# Slack
-#SLACK_WEBHOOK_URK = 'https://hooks.slack.com/services/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'
-#SLACK_USERNAME = 'Fastlane'
-#SLACK_CHANNEL = 'ios'
-
 # GITHub
 GITHUB_URL = "https://github.com/SwiftIsland/island-app/"
