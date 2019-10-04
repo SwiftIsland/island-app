a0266eaf762b8c231593967e62215202217179aa
Michael Johannes Steudter <m.steudter@campus.tu-berlin.de>
2019-10-04 17:14:06 +0200

    added icon to sketch-file, added dummy json+labels

--- SwiftIsland/About/AboutViewController.swift
+++ SwiftIsland/About/AboutViewController.swift
@@ -11,10 +11,47 @@ import UIKit
 class AboutViewController: UIViewController {
   
   private let dataManager = DataManager.shared
-  private var about: About?
+  private var about: About? {
+    didSet {
+      updateLabels()
+    }
+  }
+  
+  @IBOutlet weak var appNameLabel: UILabel!
+  @IBOutlet weak var appVersionLabel: UILabel!
+  @IBOutlet weak var appDescriptionLabel: UILabel!
+  @IBOutlet weak var appOrganisersLabel: UILabel!
+  @IBOutlet weak var eventInfoLabel: UILabel!
+  @IBOutlet weak var githubLinkButton: UIButton!
+  @IBOutlet weak var contributorsLabel: UILabel!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .red
+    loadDummyAboutData()
   }
+  
+  private func loadDummyAboutData() {
+    
+    // TODO: remove and include cacheManager etc.
+    guard let dummyData = TestDataManager().getLocalTestAsset(forPath: "About_valid.json") else { return }
+    about = try? JSONDecoder().decode(About.self, from: dummyData)
+  }
+  
+  private func updateLabels() {
+    guard let about = about else { return }
+    appNameLabel.text = about.appInfo.appName
+    appVersionLabel.text = "Version: \( about.appInfo.appVersion)"
+    appDescriptionLabel.text = "About the app:\n\(about.appInfo.appDescription)"
+    appOrganisersLabel.text = "Organisers of github repository:\n\( about.appInfo.organisers.joined(separator: ", "))"
+    eventInfoLabel.text = "About the event:\n\(about.eventInfo)"
+  githubLinkButton.setTitle(about.githubLink, for: .normal)
+    contributorsLabel.text = "Contributors: \( about.contributors.joined(separator: ", "))"
+  }
+  
+  @IBAction func openGithubUrl(_ sender: Any) {
+    // e.g. open in Safari
+    print("open safari")
+  }
+  
+  
 }
--- SwiftIsland/Entities/About.swift
+++ SwiftIsland/Entities/About.swift
@@ -9,8 +9,15 @@
 import Foundation
 
 struct About: Codable {
-  let appInfo: String
+  let appInfo: AppInfo
   let eventInfo: String
   let githubLink: String
   let contributors: [String]
+}
+
+struct AppInfo: Codable {
+  let appName: String
+  let appVersion: String
+  let appDescription: String
+  let organisers: [String]
 }
